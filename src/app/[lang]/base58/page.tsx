bcb707c8546e464ff570af5c61fcd0e58f611e38
larry <xspanni@gmail.com>
2025-07-24 18:29:16 +0800

    修复编译错误

--- src/app/[lang]/base58/page.tsx
+++ src/app/[lang]/base58/page.tsx
@@ -1,4 +1,4 @@
-export async function Page() {
+export  default  async function Page() {
     return <div>
         <h1>Base58 Encoding/Decoding</h1>
         <p>This page is under construction. Please check back later.</p>
